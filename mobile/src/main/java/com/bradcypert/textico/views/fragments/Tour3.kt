dca2ee088f3c5a2c4067074f4da2ef77b4a98a15
Brad Cypert <brad.cypert@twinspires.com>
2018-05-18 17:20:45 -0400

    setup db button

--- mobile/src/main/java/com/bradcypert/textico/views/fragments/Tour3.kt
+++ mobile/src/main/java/com/bradcypert/textico/views/fragments/Tour3.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import com.bradcypert.textico.R
+import com.bradcypert.textico.migrations.SetupDB
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class Tour3 : Fragment() {
 
@@ -24,6 +28,11 @@ class Tour3 : Fragment() {
 
         setupDbButton = view.findViewById(R.id.setup_db_button)
         setupDbButton!!.setOnClickListener {
+            SetupDB(activity).run().observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe {
+//                        Toast.makeText(activity, "You're ready to get started!", Toast.LENGTH_SHORT).show()
+                    }
             val a = parentFragment as OnDBBuiltListener
             a.onDBBuilt()
         }
