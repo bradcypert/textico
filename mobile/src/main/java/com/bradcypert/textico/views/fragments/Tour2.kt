6ad353a32011c0a0c93a4d36a821912540b2b3c3
Brad Cypert <brad.cypert@twinspires.com>
2018-05-18 17:36:23 -0400

    translations

--- mobile/src/main/java/com/bradcypert/textico/views/fragments/Tour2.kt
+++ mobile/src/main/java/com/bradcypert/textico/views/fragments/Tour2.kt
@@ -2,17 +2,13 @@ package com.bradcypert.textico.views.fragments
 
 import android.app.Fragment
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.bradcypert.textico.R
-import android.provider.Telephony.Sms.getDefaultSmsPackage
-import android.os.Build.VERSION_CODES.KITKAT
-import android.os.Build.VERSION.SDK_INT
-import android.provider.Telephony
 
 
 class Tour2 : Fragment() {
